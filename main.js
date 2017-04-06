182296a692177f502a680582b6455044b59a45fc
MarekJakimiuk1 <marekjakimiuk1@gmail.com>
2017-04-06 20:39:49 +0200

    Enlarged quantity of possible devices to control. DRY approach.

--- main.js
+++ main.js
@@ -16,26 +16,24 @@ var app = require('http').createServer(handler),
 var SerialPort = sp
 var SerialHardwarePort = 'COM14';
 var serialPort = new SerialPort(SerialHardwarePort,
-    {   baudrate: 57600,
+    {   baudrate: 9600,
         dataBits: 8,
         parity: 'none',
         stopBits: 1,
         flowControl: false
     });
 
-
 var networkInterfaces=os.networkInterfaces();
-var commonStatus = 'ON';
+var stan;
+var relay;
 
 app.listen(9000);
 
-
 serialPort.on("open", function() {
-    console.log("\n" + 'SUCCES!' + "\n" + 'Serialport has been connected to port')
+    console.log("\n" + 'SUCCES!' + "\n" + 'Serialport has been connected to port ' + SerialHardwarePort)
 });
 
 //Display my IP
-
 for(var interface in networkInterfaces) {
     networkInterfaces[interface].forEach(
         function(details){
@@ -59,34 +57,22 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('your id',
-        { id: socket.id
-        });
+        socket.emit('your id',
+        { id: socket.id });
 
-    io.sockets.emit('on connection',
+        io.sockets.emit('on connection',
         { client: socket.id, clientCount: io.sockets.clients().length,
         });
 
     socket.emit('ack button status', {
-        status: commonStatus
+        status: stan,
+        whichOne: relay
     });
 
     socket.on('button update event', function (data) {
-
-        if(data.status == 'LED_1_ON'){
-            commonStatus = 'LED_1_ON';
-            serialPort.write(commonStatus + "\n");
-        }
-
-        if(data.status == 'LED_1_OFF'){
-            commonStatus = 'LED_1_OFF';
-            serialPort.write(commonStatus + "\n");
-        }
-
-        io.sockets.emit('ack button status',
-            { status: commonStatus,
-                by: socket.id
-            });
+        //DRY
+        serialPort.write(data.whichOne+data.status+'\n');
+        io.sockets.emit('ack button status',data);
     });
 
     // Function while user left SimpleSerialPort
